f9cb81177b9e423af5fb60099fe03aa710e49604
Tom Arrow <bomarrow1@gmail.com>
2014-05-13 22:52:43 +0100

    Implement ValueParser

--- src/Api/Service/ValueParser.php
+++ src/Api/Service/ValueParser.php
@@ -3,20 +3,48 @@
 namespace Wikibase\Api\Service;
 
 use DataValues\DataValue;
+use DataValues\Deserializers\DataValueDeserializer;
+use Mediawiki\Api\MediawikiApi;
 
 /**
  * @author Adam Shorland
+ * @author Thomas Arrow
  */
 class ValueParser {
 
 	/**
-	 * @param string $value
+	 * @var MediawikiApi
+	 */
+	protected $api;
+
+	/**
+	 * @param MediawikiApi $api
+	 */
+	public function __construct( MediawikiApi $api ) {
+		$this->api = $api;
+	}
+
+	/**
+	 * @param $value
+	 * @param $parser
+	 *
+	 * @internal param string $value
 	 *
 	 * @returns DataValue
 	 */
-	public function parse( $value ) {
-		//TODO implement me
-		throw new \BadMethodCallException( 'Not yet implemented' );
+	public function parse( $value, $parser ) {
+		$result = $this->api->getAction( 'wbparsevalue', array( 'parser' => $parser, 'values' => $value ) );
+		$deserializer = new DataValueDeserializer( array(
+			'number' => 'DataValues\NumberValue',
+			'string' => 'DataValues\StringValue',
+			'globecoordinate' => 'DataValues\GlobeCoordinateValue',
+			'monolingualtext' => 'DataValues\MonolingualTextValue',
+			'multilingualtext' => 'DataValues\MultilingualTextValue',
+			'quantity' => 'DataValues\QuantityValue',
+			'time' => 'DataValues\TimeValue',
+			'wikibase-entityid' => 'Wikibase\DataModel\Entity\EntityIdValue', ) );
+		$resultobject = $deserializer->deserialize( $result["results"][0] );
+		return $resultobject;
 	}
 
 } 
