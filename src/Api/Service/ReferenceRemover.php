56363e8fcbdb56813312715251fd268fe54a0a0f
addshore <addshorewiki@gmail.com>
2014-07-04 19:10:57 +0100

    Implement ReferenceRemover
    Fixes #12

--- src/Api/Service/ReferenceRemover.php
+++ src/Api/Service/ReferenceRemover.php
@@ -3,6 +3,7 @@
 namespace Wikibase\Api\Service;
 
 use Mediawiki\Api\MediawikiApi;
+use UnexpectedValueException;
 use Wikibase\DataModel\Claim\Claim;
 use Wikibase\DataModel\Claim\ClaimGuid;
 use Wikibase\DataModel\Reference;
@@ -26,12 +27,41 @@ class ReferenceRemover {
 
 	/**
 	 * @since 0.2
+	 *
 	 * @param Reference|string $reference Reference object or hash
 	 * @param Claim|ClaimGuid|string $target Claim object or GUID
+	 *
+	 * @throws UnexpectedValueException
+	 * @return bool
 	 */
 	public function set( $reference, $target ) {
-		//TODO implement me
-		throw new \BadMethodCallException( 'Not yet implemented' );
+		if( $reference instanceof Reference ) {
+			$reference = $reference->getHash();
+		}
+		if( !is_string( $reference ) ) {
+			throw new UnexpectedValueException( 'Could not get reference hash from $reference' );
+		}
+
+		if( is_string( $target ) ) {
+			$claimGuid = $target;
+		} else if ( $target instanceof ClaimGuid ) {
+			$claimGuid = $target->getSerialization();
+		} else if ( $target instanceof Claim ) {
+			$claimGuid = $target->getGuid();
+		} else {
+			throw new UnexpectedValueException( 'Could not get claim guid from $target' );
+		}
+		if( !is_string( $claimGuid ) ) {
+			throw new UnexpectedValueException( 'Unexpected claim guid got from $target' );
+		}
+
+		$params = array(
+			'statement' => $claimGuid,
+			'references' => $reference,
+		);
+
+		$this->api->postAction( 'wbremovereferences', $params );
+		return true;
 	}
 
 } 
