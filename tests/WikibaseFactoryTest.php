f223aa9df79eb7af5a443cf48471b7903c7bc71d
addshore <addshorewiki@gmail.com>
2014-07-04 20:30:00 +0100

    Test all public methods in WikibaseFactory

--- tests/WikibaseFactoryTest.php
+++ tests/WikibaseFactoryTest.php
@@ -21,10 +21,36 @@ class WikibaseFactoryTest extends \PHPUnit_Framework_TestCase {
 		$this->assertTrue( true );
 	}
 
-	public function testNewRevisionGetter() {
+	public function provideMethodsAndClasses() {
+		return array(
+			array( 'newAliasGroupSetter','\Wikibase\Api\Service\AliasGroupSetter' ),
+			array( 'newClaimCreator','\Wikibase\Api\Service\ClaimCreator' ),
+			array( 'newClaimGetter','\Wikibase\Api\Service\ClaimGetter' ),
+			array( 'newClaimRemover','\Wikibase\Api\Service\ClaimRemover' ),
+			array( 'newClaimSetter','\Wikibase\Api\Service\ClaimSetter' ),
+			array( 'newDescriptionSetter','\Wikibase\Api\Service\DescriptionSetter' ),
+			array( 'newItemMerger','\Wikibase\Api\Service\ItemMerger' ),
+			array( 'newLabelSetter','\Wikibase\Api\Service\LabelSetter' ),
+			array( 'newReferenceRemover','\Wikibase\Api\Service\ReferenceRemover' ),
+			array( 'newReferenceSetter','\Wikibase\Api\Service\ReferenceSetter' ),
+			array( 'newRevisionGetter','\Wikibase\Api\Service\RevisionGetter' ),
+			array( 'newRevisionSaver','\Wikibase\Api\Service\RevisionSaver' ),
+			array( 'newSiteLinkLinker','\Wikibase\Api\Service\SiteLinkLinker' ),
+			array( 'newSiteLinkSetter','\Wikibase\Api\Service\SiteLinkSetter' ),
+			array( 'newValueFormatter','\Wikibase\Api\Service\ValueFormatter' ),
+			array( 'newValueParser','\Wikibase\Api\Service\ValueParser' ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideMethodsAndClasses
+	 */
+	public function testNewFactoryObject( $method, $class ) {
 		$factory = new WikibaseFactory( $this->getMockApi() );
-		$getter = $factory->newRevisionGetter();
-		$this->assertInstanceOf( '\Wikibase\Api\Service\RevisionGetter', $getter );
+
+		$this->assertTrue( method_exists( $factory, $method ) );
+		$object = $factory->$method();
+		$this->assertInstanceOf( $class, $object );
 	}
 
 } 
