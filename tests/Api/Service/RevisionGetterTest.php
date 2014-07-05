3c9d2426c0ec17d292e836c4f33e0e27e17e795b
addshore <addshorewiki@gmail.com>
2014-07-05 12:07:07 +0100

    Actually write a test for RevisionGetter

--- tests/Api/Service/RevisionGetterTest.php
+++ tests/Api/Service/RevisionGetterTest.php
@@ -3,6 +3,9 @@
 namespace Wikibase\Api\Test;
 
 use Wikibase\Api\Service\RevisionGetter;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
 
 /**
  * @covers Wikibase\Api\Service\RevisionGetter
@@ -26,6 +29,49 @@ class RevisionGetterTest extends \PHPUnit_Framework_TestCase {
 	public function testValidConstructionWorks() {
 		new RevisionGetter( $this->getMockApi(), $this->getMockDeserializer() );
 		$this->assertTrue( true );
+	}
+
+	public function provideIds() {
+		return array(
+			array( 'Q1' ),
+			array( ItemId::newFromNumber( 1 ) ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideIds
+	 */
+	public function testGetFromId( $id ) {
+		$api = $this->getMockApi();
+		$api->expects( $this->once() )
+			->method( 'getAction' )
+			->with(
+				$this->equalTo( 'wbgetentities' ),
+				$this->equalTo( array( 'ids' => 'Q1' ) )
+			)
+			->will( $this->returnValue( array( 'entities' => array( 'Q123' => array(
+				'pageid' => '111',
+				'lastrevid' => '222',
+				'modified' => 'TIMESTAMP'
+			) ) ) ) );
+		$deserializer = $this->getMockDeserializer();
+		$deserializer->expects( $this->once() )
+			->method( 'deserialize' )
+			->with( $this->equalTo( array(
+						'pageid' => '111',
+						'lastrevid' => '222',
+						'modified' => 'TIMESTAMP'
+			) ) )
+			->will( $this->returnValue( Item::newEmpty() ) );
+
+		$service = new RevisionGetter( $api, $deserializer );
+		$result = $service->getFromId( $id );
+
+		$this->assertInstanceOf( 'Mediawiki\DataModel\Revision', $result );
+		$this->assertInstanceOf( 'Wikibase\DataModel\ItemContent', $result->getContent() );
+		$this->assertInstanceOf( 'Wikibase\DataModel\Entity\Item', $result->getContent()->getNativeData() );
+		$this->assertEquals( 111, $result->getPageId() );
+		$this->assertEquals( 'TIMESTAMP', $result->getTimestamp() );
 	}
 
 } 
