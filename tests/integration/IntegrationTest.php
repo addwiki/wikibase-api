6f76a1ee73ed7be687d68f2b6bbc5f74c6d7cbaf
addshore <addshorewiki@gmail.com>
2014-07-31 17:35:35 +0100

    Split integration test into individual methods

--- tests/integration/IntegrationTest.php
+++ tests/integration/IntegrationTest.php
@@ -6,73 +6,119 @@ namespace Wikibase\Api\Test;
 use Mediawiki\Api\UsageException;
 use Mediawiki\DataModel\Revision;
 use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\DataModel\ItemContent;
 use Wikibase\DataModel\SiteLink;
 use Wikibase\DataModel\Term\AliasGroup;
 use Wikibase\DataModel\Term\Term;
 
+
+/**
+ * This test requires a wiki installed at localhost that can be edited by anon users
+ * This test also requires the sites table to be populated using populateSitesTable.php
+ */
 class IntegrationTest extends IntegrationTestBase {
 
 	/**
-	 * This test requires a wiki installed at localhost that can be edited by anon users
-	 * This test also requires the sites table to be populated using populateSitesTable.php
+	 * @var ItemId
 	 */
-	public function test() {
-		// Create a starting item
-		$localItem = Item::newEmpty();
-		$localItem->getFingerprint()->getLabels()->setTextForLanguage( 'en', 'TestItem - ' . strval( time() ) );
-		$localItem->getFingerprint()->getDescriptions()->setTextForLanguage( 'en', 'TestDescription - ' . microtime() );
+	static private $itemId;
 
-		// Save the item
-		$newItem = $this->factory->newRevisionSaver()->save( new Revision( new ItemContent( $localItem ) ) );
-		$itemId = $newItem->getId(); // Save our ID for later use
-		$localItem->setId( $itemId );
-		$this->assertTrue( $localItem->equals( $newItem ) );
+	/**
+	 * @var Item
+	 */
+	static private $localItem;
 
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+		self::$localItem = Item::newEmpty();
+		self::$localItem->getFingerprint()->getLabels()->setTextForLanguage( 'en', 'TestItem - ' . strval( time() ) );
+		self::$localItem->getFingerprint()->getDescriptions()->setTextForLanguage( 'en', 'TestDescription - ' . microtime() );
+	}
+
+	public function testCreateItem() {
+		$newItem = $this->factory->newRevisionSaver()->save( new Revision( new ItemContent( self::$localItem ) ) );
+		self::$itemId = $newItem->getId(); // Save our ID for later use
+		self::$localItem->setId( self::$itemId );
+		$this->assertTrue( self::$localItem->equals( $newItem ) );
+	}
+
+	/**
+	 * @depends testCreateItem
+	 */
+	public function testGetNewlyCreatedItem() {
 		// Make sure the RevisionGetter will also return the same Item as expected
-		$gotItem = $this->factory->newRevisionGetter()->getFromId( $itemId )->getContent()->getNativeData();
-		$this->assertTrue( $localItem->equals( $gotItem ) );
+		$gotItem = $this->factory->newRevisionGetter()->getFromId( self::$itemId )->getContent()->getNativeData();
+		$this->assertTrue( self::$localItem->equals( $gotItem ) );
+	}
 
-		// Set a label in de
+	/**
+	 * @depends testCreateItem
+	 */
+	public function testSetLabel() {
 		$labelDe = new Term( 'de', 'Foo' . microtime() );
-		$r = $this->factory->newLabelSetter()->set( $labelDe , $itemId );
+		$r = $this->factory->newLabelSetter()->set( $labelDe , self::$itemId );
 		$this->assertTrue( $r );
-		$localItem->getFingerprint()->getLabels()->setTerm( $labelDe );
+		self::$localItem->getFingerprint()->getLabels()->setTerm( $labelDe );
+	}
 
-		// Set a desc in en-gb
+	/**
+	 * @depends testCreateItem
+	 */
+	public function testSetDescription() {
 		$descDe = new Term( 'de', 'FooBarDesc' . microtime() );
-		$r = $this->factory->newDescriptionSetter()->set( $descDe, $itemId );
+		$r = $this->factory->newDescriptionSetter()->set( $descDe, self::$itemId );
 		$this->assertTrue( $r );
-		$localItem->getFingerprint()->getDescriptions()->setTerm( $descDe );
+		self::$localItem->getFingerprint()->getDescriptions()->setTerm( $descDe );
+	}
 
-		// Set 2 aliases in fr
+	/**
+	 * @depends testCreateItem
+	 */
+	public function testSetAliases() {
 		try{
 			$aliasFr = new AliasGroup( 'fr', array( 'aa', 'bb' ) );
-			$r = $this->factory->newAliasGroupSetter()->set( $aliasFr, $itemId );
+			$r = $this->factory->newAliasGroupSetter()->set( $aliasFr, self::$itemId );
 			$this->assertTrue( $r );
-			$localItem->getFingerprint()->getAliasGroups()->setGroup( $aliasFr );
+			self::$localItem->getFingerprint()->getAliasGroups()->setGroup( $aliasFr );
+			$this->markTestIncomplete( 'This is no longer a bug!' );
 		}
 		catch( UsageException $e ) {
 			// Wikibase bug
+			$this->markTestSkipped( 'Wikibase bug: ' . $e->getMessage() );
 		}
+	}
 
-		// Set a sitelink for enwiki
+	/**
+	 * @depends testCreateItem
+	 */
+	public function testSetSitelink() {
 		$enwikiLondon = new SiteLink( 'enwiki', 'London' );
-		$r = $this->factory->newSiteLinkSetter()->set( $enwikiLondon, $itemId );
+		$r = $this->factory->newSiteLinkSetter()->set( $enwikiLondon, self::$itemId );
 		$this->assertTrue( $r );
-		$localItem->getSiteLinkList()->addSiteLink( $enwikiLondon );
+		self::$localItem->getSiteLinkList()->addSiteLink( $enwikiLondon );
+	}
 
-		// Link another sitelink using the last
+	/**
+	 * @depends testCreateItem
+	 * @depends testSetSitelink
+	 */
+	public function testLinkSitelinks() {
+		$enwikiLondon = new SiteLink( 'enwiki', 'London' );
 		$dewikiBerlin = new Sitelink( 'dewiki', 'Berlin' );
 		$r = $this->factory->newSiteLinkLinker()->link( $enwikiLondon, $dewikiBerlin );
 		$this->assertTrue( $r );
-		$localItem->getSiteLinkList()->addSiteLink( $dewikiBerlin );
+		self::$localItem->getSiteLinkList()->addSiteLink( $dewikiBerlin );
+	}
 
-		// Empty the item
-		$localItem = Item::newEmpty();
-		$localItem->setId( $itemId );
-		$newItem = $this->factory->newRevisionSaver()->save( new Revision( new ItemContent( $localItem ) ) );
-		$this->assertTrue( $localItem->equals( $newItem ) );
+	/**
+	 * @depends testCreateItem
+	 */
+	public function testEmptyItem() {
+		self::$localItem = Item::newEmpty();
+		self::$localItem->setId( self::$itemId );
+		$newItem = $this->factory->newRevisionSaver()->save( new Revision( new ItemContent( self::$localItem ) ) );
+		$this->assertTrue( self::$localItem->equals( $newItem ) );
 	}
 
 }
