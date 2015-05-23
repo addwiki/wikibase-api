4e239756fb731402d75fb84c9dc360d407ce2d05
Tpt <thomaspt@hotmail.fr>
2015-05-23 13:30:19 +0200

    Adds PropertyApiLookup

---
+++ src/Api/PropertyApiLookup.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Wikibase\Api;
+
+use Wikibase\Api\Service\RevisionGetter;
+use Wikibase\DataModel\Entity\PropertyId;
+use Wikibase\DataModel\Entity\PropertyLookup;
+use Wikibase\DataModel\Entity\PropertyNotFoundException;
+
+/**
+ * @author Thomas Pellissier Tanon
+ */
+class PropertyApiLookup implements PropertyLookup {
+
+	/**
+	 * @var RevisionGetter
+	 */
+	private $revisionGetter;
+
+	/**
+	 * @param RevisionGetter $revisionGetter
+	 */
+	public function __construct( RevisionGetter $revisionGetter ) {
+		$this->revisionGetter = $revisionGetter;
+	}
+
+	/**
+	 * @see PropertyLookup::getPropertyForId
+	 */
+	public function getPropertyForId( PropertyId $itemId ) {
+		$revision = $this->revisionGetter->getFromId( $itemId );
+
+		if( !$revision ) {
+			throw new PropertyNotFoundException( $itemId );
+		}
+
+		return $revision->getContent()->getData();
+	}
+}
